9c945fedc3d4f68ad8740bd0f3e18bc26e4b6b45
shannonbux <32467162+shannonbux@users.noreply.github.com>
2019-11-12 14:36:56 -0800

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -19,7 +19,7 @@ class BlogIndex extends React.Component {
             Blog
           </div>
           <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
+            <h1 className="section-headline">Recent articles</h1>
             <ul className="article-list">
               {posts.map(({ node }) => {
                 return (
